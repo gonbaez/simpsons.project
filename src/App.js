76e450a8e260274ecb54ada246bc4b74b07ac3b7
unknown <gon.baez@gmail.com>
2024-02-07 18:04:24 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from "axios";
+
+import styles from "./styles/App.module.css";
+
+class App extends Component {
+  state = {};
+
+  async componentDidMount() {
+    const response = await axios.get(
+      "https://thesimpsonsquoteapi.glitch.me/quotes?count=50"
+    );
+
+    console.log(response);
+  }
+
+  render() {
+    return (
+      <>
+        <header>
+          <img src="the_simpsons.png" alt="The Simpsons" />
+        </header>
+        <main>
+          <section className={styles.simpsonsLogo}></section>
+          <section className={styles.tvContainer}>
+            <img src="static-noise.jpg" />
+          </section>
+        </main>
+
+        <footer>
+          <p>&copy; Gonzalo Baez 2024</p>
+        </footer>
+      </>
+    );
+  }
 }
 
 export default App;
